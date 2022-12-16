a86bf45483ff89073805c0ce68faa9ecb286e534
Ashabenny123 <110253397+Ashabenny123@users.noreply.github.com>
2022-12-16 10:51:00 +0530

    mobile fix

--- src/js/index.js
+++ src/js/index.js
@@ -1,19 +1,27 @@
-import { preloadImages, preloadFonts } from './utils';
-import LocomotiveScroll from 'locomotive-scroll';
+import { preloadImages, preloadFonts } from "./utils";
+import LocomotiveScroll from "locomotive-scroll";
 
-const backtopEl = document.querySelector('.backtop');
-const headerEl = document.querySelector('#header');
+const backtopEl = document.querySelector(".backtop");
+const headerEl = document.querySelector("#header");
 
 // Preload  images and fonts
-Promise.all([preloadImages('.tiles__line-img'), preloadFonts('rmd7deq')]).then(() => {
+Promise.all([preloadImages(".tiles__line-img"), preloadFonts("rmd7deq")]).then(
+  () => {
     // Remove loader (loading class)
-    document.body.classList.remove('loading');
+    document.body.classList.remove("loading");
 
     // Initialize the Locomotive scroll
     const scroll = new LocomotiveScroll({
-        el: document.querySelector('[data-scroll-container]'),
-        smooth: true
+      el: document.querySelector("[data-scroll-container]"),
+      smooth: true,
+      mobile: {
+        smooth: true,
+      },
+      tablet: {
+        smooth: true,
+      },
     });
 
-    backtopEl.addEventListener('click', () => scroll.scrollTo(headerEl));
-});
+    backtopEl.addEventListener("click", () => scroll.scrollTo(headerEl));
+  }
+);
