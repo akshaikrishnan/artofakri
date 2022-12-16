185b2d2ad909835d9dcd44f6ff44ea5845861e5d
Ashabenny123 <110253397+Ashabenny123@users.noreply.github.com>
2022-12-16 12:23:59 +0530

    og

--- src/js/index.js
+++ src/js/index.js
@@ -24,6 +24,26 @@ Promise.all([preloadImages(".tiles__line-img"), preloadFonts("rmd7deq")]).then(
         smooth: true,
       },
     });
+    const thumbnailArray = [
+      "https://artofakri.firebaseapp.com/aksha_ai_57.97cc8bf3.jpeg",
+      "https://artofakri.firebaseapp.com/aksha_ai_58.da42ba32.jpeg",
+      "https://artofakri.firebaseapp.com/aksha_ai_50.9d639de4.jpeg",
+      "https://artofakri.firebaseapp.com/aksha_ai_46.f0e5a335.jpeg",
+      "https://artofakri.firebaseapp.com/aksha_ai_81.77102bbb.jpeg",
+      "https://artofakri.firebaseapp.com/aksha_ai_59.57eeae95.jpeg",
+    ];
+
+    // get a reference to the thumbnail element
+    const thumbnailElement = document.getElementById("random-thumbnail");
+
+    // generate a random index based on the length of the thumbnailArray
+    const randomIndex = Math.floor(Math.random() * thumbnailArray.length);
+
+    // select the meta element with the property "og:title"
+    const metaElement = document.querySelector('meta[property="og:image"]');
+
+    // set the content attribute of the meta element to the desired value
+    metaElement.content = thumbnailArray[randomIndex];
 
     backtopEl.addEventListener("click", () => scroll.scrollTo(headerEl));
   }
